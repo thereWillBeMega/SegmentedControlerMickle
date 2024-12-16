fe62146c86c752f979c603fe4510fcba041b7a92
Peter Mickle <robloxpro408@gmail.com>
2024-12-15 21:28:51 -0600

    Initial Commit

---
+++ SegmentedControlerMickle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControlerMickle
+//
+//  Created by Peter Mickle on 12/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SegmentedControlerMickleTests/SegmentedControlerMickleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlerMickleTests.swift
+//  SegmentedControlerMickleTests
+//
+//  Created by Peter Mickle on 12/15/24.
+//
+
+import Testing
+@testable import SegmentedControlerMickle
+
+struct SegmentedControlerMickleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
