01ea2de1d518d0cb543977b1af446ef4a190e058
Peter Mickle <robloxpro408@gmail.com>
2024-12-15 21:38:45 -0600

    wow :D

--- SegmentedControlerMickle/ViewController.swift
+++ SegmentedControlerMickle/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        var select = sender.selectedSegmentIndex
+        
+        switch select {
+        case 0:
+            print("selected 1")
+        case 1:
+            print("selected 2")
+        case 2:
+            print("selected 3")
+        default:
+            print("you didnt select anything :(")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
